baa11bcedb98bb8410c5ad2d12b362a43823eab1
Ramon Chavez <rachavez1974@gmail.com>
2019-09-30 14:53:20 -0400

    Finish js-basics-arithmetic-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+
+const newID = 1000000000 + oldID;
+const ageIsValid = Number.isInteger();
+const randomNumber = Math.random()
+const randomInteger = Math.floor(Math.random())
+const randomUserID = Math.floor(Math.random() * (1000000020 - 1000000001) + 1000000001);
+
